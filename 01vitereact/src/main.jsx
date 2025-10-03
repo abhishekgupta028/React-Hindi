fce6faea0c522810cced12b069118602a55e29a4
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-10-03 17:38:54 +0530

    custom react added

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -11,14 +11,14 @@ function MyApp(){
   )
 }
 
-const ReactElement = {
-  type: 'a',
-  props: {
-    href: 'https://google.com',
-    target: '_blank'
-  },
-  children: 'Click me to visit google'
-}
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
 
 const anotherElement = (
   <a href="https://google.com" target="_blank">Visit Google</a>
