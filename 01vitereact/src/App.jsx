3d4fa2fd9e8d2d691c87667964c9ba22df4c8ad8
abhishekgupta028 <abhishekgupta6205691283@gmail.com>
2025-10-03 17:30:17 +0530

    custom react added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai";
+
+function App() {
+    const username = "chai aur code"
+
+  return (
+    <>
+      <Chai />
+      <h1>Chai aur React on {username} | Abhishek Kumar</h1>
+      <p>Hi, {username}</p>
+    </>
+  );
+}
+
+export default App;
